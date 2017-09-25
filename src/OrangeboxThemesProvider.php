3a4d35b75d5c0fb3bf228b2b8d0807d9c7eb0910
shawnsandy04 <shawnsandy04@gmail.com>
2017-09-24 22:20:27 -0400

    WIP : package files

--- src/OrangeboxThemesProvider.php
+++ src/OrangeboxThemesProvider.php
@@ -5,9 +5,6 @@ namespace ShawnSandy\Orangebox;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory;
 
-
-
-
 /**
 * Class Provider
  * @package ShawnSandy\OrangeBox
@@ -17,134 +14,129 @@ class OrangeboxThemesProvider extends ServiceProvider
 
 
 
-	/**
-	* Perform post-registration booting of services.
-			     *
-			     * @return void
-			     */
-			    public function boot()
-			    {
+    /**
+    * Perform post-registration booting of services.
+                 *
+                 * @return void
+                 */
+    public function boot()
+    {
 
 
 
 
 
-		/**
-		* Package views
-						         */
-						        $this->loadViewsFrom(__DIR__ . '/resources/views', 'orangebox');
-		$this->publishes(
-						            [
-						                __DIR__ . '/resources/views' => resource_path('views/vendor/orangebox'),
-						            ], 'orangebox-views'
-						        );
+        /**
+        * Package views
+                                 */
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'orangebox');
+        $this->publishes(
+            [
+                __DIR__ . '/resources/views' => resource_path('views/vendor/orangebox'),
+            ], 'orangebox-views'
+        );
 
 
 
 
-		/**
-		* Package assets
-						         */
-						        $this->publishes(
-						            [
-						                __DIR__.'/resources/assets/js/' => public_path('assets/orangebox/js/'),
-						                __DIR__.'/public/assets/' => public_path('assets/')
-						            ], 'orangebox-assets'
-						        );
+        /**
+        * Package assets
+                                 */
+        $this->publishes(
+            [
+                __DIR__.'/resources/assets/js/' => public_path('assets/orangebox/js/'),
+                __DIR__.'/public/assets/' => public_path('assets/'),
+                __DIR__.'/public/' => public_path('orangebox'),
+            ], 'orangebox-assets'
+        );
 
 
 
 
 
-		/**
-		* Package resources to resources
-						         */
-						        $this->publishes(
-						            [
-						                __DIR__.'/resources/assets/' => resource_path('assets/orangebox/'),
-						            ], 'orangebox-resources'
-						        );
+        /**
+        * Package resources to resources
+                                 */
+        $this->publishes(
+            [
+                __DIR__.'/resources/assets/' => resource_path('assets/orangebox/'),
+            ], 'orangebox-resources'
+        );
 
 
 
 
-		/**
-		* Package config
-						         */
-						        $this->publishes(
-						            [__DIR__ . '/config/config.php' => config_path('orangebox.php')],
-						            'orangebox-config'
-						        );
+        /**
+        * Package config
+                                 */
+        $this->publishes(
+            [__DIR__ . '/config/config.php' => config_path('orangebox.php')],
+            'orangebox-config'
+        );
 
-		$this->publishes([
-								            __DIR__ . '/migrations/' => database_path('migrations')
-								        ], 'orangebox-migrations');
-
-
-		$this->loadMigrationsFrom(__DIR__ . '/migrations');
-		$this->app->bind(
-				        'Orangebox', function () {
-			return new Orangebox();
-		}
-		);
-
-		$this->registerFactoriesPath(__DIR__.'/factories');
-
-		$views = resource_path(
-		            "views/orangebox"
-		        );
-
-		$this->loadViewsFrom($views, 'orangebox');
+        $this->publishes([
+                    __DIR__ . '/migrations/' => database_path('migrations')
+                ], 'orangebox-migrations');
 
 
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->app->bind(
+        'Orangebox', function () {
+            return new Orangebox();
+        }
+        );
 
-	}
+        $this->registerFactoriesPath(__DIR__.'/factories');
+
+        $views = resource_path(
+        "views/orangebox"
+        );
+
+        $this->loadViewsFrom($views, 'orangebox');
+    }
 
 
 
 
-	/**
-	* Register factories.
-		    *
-		    * @param  string  $path
-		    * @return void
-		    */
-		    protected function registerFactoriesPath($path)
-		    {
-		$this->app->make(Factory::class)->load($path);
-	}
+    /**
+    * Register factories.
+            *
+            * @param  string  $path
+            * @return void
+            */
+    protected function registerFactoriesPath($path)
+    {
+        $this->app->make(Factory::class)->load($path);
+    }
 
 
-	public function loadProviders()
-		        {
+    public function loadProviders()
+    {
 
-		if (!$this->app->runningInConsole()) :
-						            include_once __DIR__ . '/Helpers/helper.php';
-		endif;
-
-
-	}
+        if (!$this->app->runningInConsole()) :
+                                    include_once __DIR__ . '/Helpers/helper.php';
+        endif;
+    }
 
 
 
 
-	/**
-	* Register any package services.
-			     *
-			     * @return void
-			     */
-			    public function register()
-			    {
+    /**
+    * Register any package services.
+                 *
+                 * @return void
+                 */
+    public function register()
+    {
 
-		$this->mergeConfigFrom(
-						            __DIR__ . '/config/config.php', 'orangebox'
-						        );
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/config.php', 'orangebox'
+        );
 
-		$this->app->bind(
-						        'Orangebox', function () {
-			return new Orangebox();
-		}
-		);
-	}
-
+        $this->app->bind(
+        'Orangebox', function () {
+            return new Orangebox();
+        }
+        );
+    }
 }
